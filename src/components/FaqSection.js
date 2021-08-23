0ae56fd6f463978c942e83e808b75184385fcd76
George <figoto660@gmail.com>
2021-08-22 23:18:31 -0400

    add Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any questions?<span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className='answer'>
+                    <p>Dolor ea nostrud exercitation sunt sit esse. Fugiat ut et minim reprehenderit incididunt sit id. Dolore sit et eu laborum laborum elit culpa. Minim elit enim mollit non non ex elit non aliqua anim. Exercitation amet aliqua sit nulla ea.</p>
+                    <p>Consectetur fugiat eu nulla non ut consectetur fugiat minim proident.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className='answer'>\
+                    <p>Enim dolore consequat veniam minim labore excepteur laboris exercitation aute enim Lorem nulla.</p>
+                    <p>Dolor ea nostrud exercitation sunt sit esse. Fugiat ut et minim reprehenderit incididunt sit id. Dolore sit et eu laborum laborum elit culpa. Minim elit enim mollit non non ex elit non aliqua anim. Exercitation amet aliqua sit nulla ea.</p>
+                    <p>Consectetur fugiat eu nulla non ut consectetur fugiat minim proident.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>How Do I Cancel?</h4>
+                <div className='answer'>
+                    <p>Dolor ea nostrud exercitation sunt sit esse. Fugiat ut et minim reprehenderit incididunt sit id. Dolore sit et eu laborum laborum elit culpa. Minim elit enim mollit non non ex elit non aliqua anim. Exercitation amet aliqua sit nulla ea.</p>
+                    <p>Consectetur fugiat eu nulla non ut consectetur fugiat minim proident.</p>
+                    <p>Occaecat excepteur esse proident esse non consequat.</p>
+                    <p>Nisi labore enim do consectetur esse exercitation reprehenderit ullamco duis dolor consectetur excepteur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>How Do I Stop?</h4>
+                <div className='answer'>
+                    <p>Dolor ea nostrud exercitation sunt sit esse. Fugiat ut et minim reprehenderit incididunt sit id. Dolore sit et eu laborum laborum elit culpa. Minim elit enim mollit non non ex elit non aliqua anim. Exercitation amet aliqua sit nulla ea.</p>
+                    <p>Consectetur fugiat eu nulla non ut consectetur fugiat minim proident.</p>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default FaqSection;

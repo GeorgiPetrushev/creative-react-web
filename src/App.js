6b89201bd0df7f116379823765bed6b12349adce
George <figoto660@gmail.com>
2021-08-24 01:29:53 -0400

    5.Style front page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from './components/GlobalStyle';
 
 export default function App() {
   return (
     <div className="App">
+        <GlobalStyle/>
         <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,27 +1,56 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 export default function AboutSection() {
     return(
-        <div>
-            <div className='description'>
+        <About>
+            <Description>
                 <div className='title'>
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>you <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any photography onr videography ideas that you have. We have protectional whit amazing skills</p>
                 <button>Contact us.</button>
-            </div>
+            </Description>
             <div className='image'>
                 <img alt='dude whit camera' src={home1}></img>
             </div>
-        </div>
+        </About>
     )
 }
+
+const About = styled.div`
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+`
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`
+const Hide =styled.div`
+    overflow: hidden;
+`

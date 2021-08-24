ddcc1bd29f8940ac435c55337258172524bea5e7
George <figoto660@gmail.com>
2021-08-24 19:41:22 -0400

    7. Add Style to front page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 
 export default function App() {
   return (
     <div className="App">
         <GlobalStyle/>
+        <Nav />
         <AboutUs />
     </div>
   );
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 //Page Components
+
 import AboutSection from '../components/AboutSection';
 import FaqSection from '../components/FaqSection';
 import ServicesSection from '../components/ServicesSection';

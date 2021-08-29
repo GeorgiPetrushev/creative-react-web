4fd4d33a4cc8eaf88ac1f9d4c34502e71e5ecc87
George <figoto660@gmail.com>
2021-08-29 10:53:30 -0400

    10.Addig in Our Work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+//Img
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
+
 
 const OurWork = () =>{
     return(
-        <h1>Our Work</h1>
+
+            <Work>
+                <Movie>
+                    <h2>The Athlete</h2>
+                    <div className='line'></div>
+                    <Link>
+                        <img src={athlete} alt='athlete'></img>
+                    </Link>
+                </Movie>
+                <Movie>
+                    <h2>The Athlete</h2>
+                    <div className='line'></div>
+                    <Link>
+                    <img src={theracer} alt='athlete'></img>
+                    </Link>
+                </Movie>
+                <Movie>
+                    <h2>The Athlete</h2>
+                    <div className='line'></div>
+                    <Link>
+                        <img src={goodtimes} alt='athlete'></img>
+                    </Link>
+                </Movie>
+            </Work>    
     )
 }
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2{
+        padding: 1rem 0rem;
+    }
+`;
+const Movie = styled.div`
+    padding-bottom: 10rem;
+    .line{
+        height: 0.5rem;
+        background-color: #cccccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`;
+
 
 export default OurWork;

924a9bff4b166e380fa362c1b3a24aed93f68a3e
George <figoto660@gmail.com>
2021-09-01 13:44:37 -0400

    12.Addning motion on Tittle text

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,22 +2,38 @@ import React from 'react';
 import home1 from '../img/home1.png';
 // import styled from 'styled-components';
 import { About,Description,Hide,Image } from '../style';
+import { motion } from 'framer-motion';
 
 export default function AboutSection() {
+
+    const titleAnimation = {
+        hidden: {opacity: 0, x:50},
+        transition: {duration:1.5},
+        show: {opacity: 1, x:0 ,transition: {duration:0.75}}
+    }
+    const container = {
+        
+        show: { transition:{ duration: 1.5, ease: 'easeOut' , staggerChildren: 1}} 
+    }
     return(
         <About>
             <Description>
-                <div className='title'>
+                <motion.div 
+                    variants={container}
+                    initial="hidden"
+                    animate="show"
+                    className="tittle"
+                >
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>you <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnimation}>you <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnimation}>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography onr videotape ideas that you have. We have protectional whit amazing skills</p>
                 <button>Contact us.</button>
             </Description>

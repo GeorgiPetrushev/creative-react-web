b1b84ccb5f6c3d0cdf81a6d15cb86bac40b1bf37
George <figoto660@gmail.com>
2021-09-05 23:32:08 -0400

    16. Our Work Sliding Animation

--- src/animation.js
+++ src/animation.js
@@ -1,25 +1,25 @@
 export const pageAnimation = {
     hidden: {
-        opacity: 0,
-        y: 300
+      opacity: 0,
+      y: 300,
     },
     show: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 1,
-            when: 'beforeChildren',
-            staggerChildren: 0.25,
-        },
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.25,
+      },
     },
     exit: {
-        opacity: 0,
         y: 300,
+        opacity: 0,
         transition: {
-            duration: 0.75,
-        },
+        duration: 0.5,
+      },
     },
-};
+  };
 
 export const titleAnimation = {
     hidden:{ 
@@ -46,8 +46,27 @@ export const photoAnimation = {
         scale: 1,
         opacity: 1,
         transition: {
-            duration: 0.75,
-            ease: 'easeOut'
+            duration: 1.75,
         },
     }
 }
+export const lineAnimation = {
+    hidden: {width: '0%'},
+    show: {width: '100%', 
+            transition: {
+                duration: 1.75, 
+            },
+    }
+}
+export const slider = {
+    hidden: { x: "-130%", skew: "45deg" },
+    show: {
+      x: "100%",
+      skew: "0deg",
+      transition: { ease: "easeOut", duration: 1.5 },
+    },
+  };
+  export const sliderContainer = {
+    hidden: { opacity: 1 },
+    show: { opacity: 1, transition: { staggerChildren: 0.15 } },
+  };
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,29 +7,37 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 // Add Animation
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import {  sliderContainer, slider, pageAnimation, fade, photoAnimation, lineAnimation } from '../animation';
 
 
 const OurWork = () =>{
     return(
 
-            <Work style={{backgroundColor: 'white'}} variants={pageAnimation} initial="hidden" animate="show" exit="exit">
+            <Work  variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{backgroundColor: 'white'}}>
+                <motion.div variants={sliderContainer}>
+                    <Frame1 variants={slider}></Frame1>
+                    <Frame2 variants={slider}></Frame2>
+                    <Frame3 variants={slider}></Frame3>
+                    <Frame4 variants={slider}></Frame4>
+                </motion.div>
                 <Movie>
-                    <h2>The Athlete</h2>
-                    <div className='line'></div>
+                    <motion.h2 variants={fade}>The Athlete</motion.h2>
+                    <motion.div variants={lineAnimation} className='line'></motion.div>
                     <Link to="/work/the-athlete">
-                        <img src={athlete} alt='athlete'></img>
+                        <Hide>
+                            <motion.img variants={photoAnimation} src={athlete} alt='athlete'></motion.img>
+                        </Hide>
                     </Link>
                 </Movie>
                 <Movie>
-                    <h2>The Athlete</h2>
+                    <motion.h2>The Racer</motion.h2>
                     <div className='line'></div>
                     <Link to="/work/the-racer">
                     <img src={theracer} alt='athlete'></img>
                     </Link>
                 </Movie>
                 <Movie>
-                    <h2>The Athlete</h2>
+                    <motion.h2>The Athlete</motion.h2>
                     <div className='line'></div>
                     <Link to="/work/good-times">
                         <img src={goodtimes} alt='athlete'></img>
@@ -50,8 +58,8 @@ const Work = styled(motion.div)`
 const Movie = styled.div`
     padding-bottom: 10rem;
     .line{
-        height: 0.5rem;
-        background-color: #cccccc;
+        height: 0.7rem;
+        background-color: #23d997;
         margin-bottom: 3rem;
     }
     img{
@@ -60,6 +68,26 @@ const Movie = styled.div`
         object-fit: cover;
     }
 `;
-
+const Hide = styled.div`
+    overflow:hidden;
+`;
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 3;
+`;
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
+`;
 
 export default OurWork;
